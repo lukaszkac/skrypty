f0485e5d4c091cca961064c0a9e02ae5e9f58d7a
Łukasz Kąc <lukaszkkac@gmail.com>
2013-03-04 12:02:40 +0100

    Poprawiono nazwę pliku do programu

--- baza/baza.sh
+++ baza/baza.sh
@@ -16,45 +16,41 @@ case $zmienna in
 	echo "s - sprawdzenie sumy kontrolnej md5";;
   "w")  clear
 	echo "W bazie danych mamy:" 
-	cat baza.sh ;;
+	cat baza.txt ;;
   "q")  clear
 	echo "Dziękujemy i zapraszamy ponownie"
 	exit;;
   "t")  echo $c;;
   "a")  clear
-	ost=`wc -l baza.sh | awk '{print $1}'`
+	ost=`wc -l baza.txt | awk '{print $1}'`
 	nas=$(($ost+1))
 	echo "Podaj imię, nazwisko, Adres IP "
 	read d
 	echo "Wpis został dodany"
-	echo "$nas. $d" >> baza.sh;;
+	echo "$nas. $d" >> baza.txt;;
   "r")  clear
 	echo "Podaj który wpis ma zostać usunięty"
 	read g
-	nrwpisu=`grep ^$d baza.sh`
+	nrwpisu=`grep ^$d baza.txt`
 	clear
 	echo "Chcesz usunąć wpis (y/n)"
 	echo "$nrwpisu"
 	read d
 		case $d in
 		  "y"|"t"|"TAK"|"tak")	clear
-					sed -i "${g}d" baza.sh 
+					sed -i "${g}d" baza.txt 
 					echo "Wpis $nrwpisu usunięty";;
 		  "n"|"NIE"|"nie")  clear
 			echo "Zmiany NIE wprowadzone"
 		esac;;
   "z")  clear
-	md5sum baza.sh > baza.md5
+	md5sum baza.txt > baza.md5
 	echo "Suma kontrolan wygenerowana";;
   "s")  clear
 	md5sum baza.md5 -c;;
   *)    clear
 	echo "Wpisałeś $zmienna. Nie wiem co to jest"
 esac
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> 1441730ec5c0ea15f79abcbd81084357d7ffcde2
 read zmienna
 done

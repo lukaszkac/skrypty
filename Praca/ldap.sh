dd646b5551b49f57b1b7bb2217775053d5c2d385
Łukasz Kąc <lukaszkac@lkac.(none)>
2013-04-24 16:05:24 +0200

    dodanie cms2

--- Praca/ldap.sh
+++ Praca/ldap.sh
@@ -6,8 +6,10 @@ read -p LDAP\> option active
 case $option in
   "h")  
 	echo "Pomoc";;
-  "kreator|kr")
-	echo "vim cn=kreator,cn=activekreator,ou=services,uid=$active,dc=nazwa,dc=pl,o=netart"
+  "kreator")
+	echo "vim cn=kreator,cn=activekreator,ou=services,uid=$active,dc=nazwa,dc=pl,o=netart";;
+  "cms2")
+	echo "vim cn=strona,cn=activeCms2,ou=services,uid=$active,dc=nazwa,dc=pl,o=netart";;
 esac
 
 #echo "vim cn=kreator,cn=activekreator,ou=services,uid=$active,dc=nazwa,dc=pl,o=netart $option"

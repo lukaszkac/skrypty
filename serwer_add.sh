bea77d97e6355c3087ec424d20358ca545fcf13a
Łukasz Kąc <lukaszkkac@gmail.com>
2013-05-08 11:24:39 +0200

    Ogarnięcie katalogu i dodanie skryptu init montującego serwery po sshfs

--- serwer_add.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# SSHFD serwerów
-echo "Montowanie serwera Provider"
-echo FATALITY1a | sshfs admin@tarzan.serwery.pl:/ /home/lukaszkac/serwery/provider -o workaround=rename -o password_stdin
-echo "Montowanie serwera Active"
-echo FATALITY1a | sshfs tarzan@tarzan.nazwa.pl:/.file /home/lukaszkac/serwery/active -o workaround=rename -o password_stdin
